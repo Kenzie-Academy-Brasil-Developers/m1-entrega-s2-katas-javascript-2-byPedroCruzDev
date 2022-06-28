e78a7406493568c5996b9d79378ea3463aa35577
[Pedro Cruz] <contatocreed67@gmail.com>
2022-06-28 09:42:42 -0400

    done

--- katas2.js
+++ katas2.js
@@ -9,8 +9,11 @@ console.assert(add(3, 5) === 8, 'A função add não está funcionando como espe
 
 // comece a criar a sua função multiply na linha abaixo
 function multiply(n1, n2){
-
-    return n1 * n2
+    let multiplyy = 0;
+    for(let i = 0; i < n2; i++){
+        multiplyy += add(0, n1)
+    }
+    return multiplyy
 
 }
 console.log(multiply(4,6))
@@ -21,9 +24,15 @@ console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionand
 
 // comece a criar a sua função power na linha abaixo
 function power (n1, n2 ){
-    return n1 ** n2
+    let power1 = 1;
+
+    for(let i = 0; i < n2; i++){
+        power1 = multiply(n1,power1)
+        //a cada loop do n1 recebe 1(power1)
+    }
+    return power1
 }
-console.log(power(3,5))
+console.log(power(3,4))
 
 // descomente a linha seguinte para testar sua função
 console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
@@ -31,11 +40,11 @@ console.assert(power(3, 4) === 81, 'A função power não está funcionando como
 
 // comece a criar a sua função factorial na linha abaixo
 function factorial(n1){
-    let result = n1;
-    for(let index = 1; index < n1; index++){
-        result *= index
+    let fac = 1;
+    for(let i = 1; i <= n1; i++){
+        fac = multiply(fac, i)
     }
-    return result
+    return fac
 }
 console.log(factorial(5))
 
@@ -49,9 +58,20 @@ console.assert(factorial(5) === 120, 'A função factorial não está funcionand
 
 // crie a função fibonacci
 function fibonacci (n) {
+    if(n < 1) return 0
+    if(n <= 2) return 1
 
-    return (n + n) -1
-}
+    let fibP2  = 0;
+    let fibN1 = 1;
+    let num = n;
+
+    for(let i = 2; i <= n; i++){
+       num = add(fibN1, fibP2)
+       fibP2 = fibN1
+       fibN1 = num
+    }
+    return num
+}  
 console.log(fibonacci(7))
 
 
